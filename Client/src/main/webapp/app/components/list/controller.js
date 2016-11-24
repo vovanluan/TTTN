722842c4fa73b030bec0723f0cf00f16a9a15373
Luan <vovanluan201294@gmail.com>
2016-11-24 23:04:00 +0700

    Update search

--- Client/src/main/webapp/app/components/list/controller.js
+++ Client/src/main/webapp/app/components/list/controller.js
@@ -10,7 +10,7 @@ app.controller('listViewController', function ($rootScope, $scope, $filter, requ
             case 'DA_CHUYEN':
                 return 'ĐANG XỬ LÝ';
             case 'DA_XU_LY':
-                return 'ĐÃ XỬ LÝ';
+                return 'ĐÃ GIẢI QUYẾT';
             case 'DA_DUYET':
                 return 'ĐÃ DUYỆT';
         }
@@ -19,14 +19,14 @@ app.controller('listViewController', function ($rootScope, $scope, $filter, requ
       return (commentRequestId==serviceRequestId);
     }
 
-    $scope.createMap = function (){
+    $scope.createMap = function (requests){
         var iconBase = "assets/resources/markerIcon/";
         $scope.map = new google.maps.Map(document.getElementById('mainMap'), {
             zoom: 10,
             center: myLatLng
         });
         $scope.markers = [];
-        $.each($rootScope.requests, function(index, request) {
+        $.each(requests, function(index, request) {
           var latlng = new google.maps.LatLng(request.latitude, request.longitude);
           var icon = "";
           switch(request.statusId) {
@@ -77,31 +77,59 @@ app.controller('listViewController', function ($rootScope, $scope, $filter, requ
 
     initController();
 
+    // init the filtered items
+    $scope.search = function () {
+        $scope.filteredRequests = $filter('filter')($rootScope.requests, function (req) {
+            for(var attr in req) {
+                if (searchMatch(req[attr], $scope.searchInput))
+                    return true;
+            }
+            return false;
+        });
+        $scope.setPage(1, $scope.filteredRequests);
+    };
+
+    var searchMatch = function (haystack, needle) {
+        if (!needle) {
+            return true;
+        }
+        return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
+    };
+
     function initController() {
         requestManager.loadAllRequests().then(function (requests){
             $rootScope.requests = requests;
               // functions have been describe process the data for display
-            $scope.setPage(1);
-            $scope.createMap();
-            $scope.$watch('requests', function (newVal, oldVal) {
-                $scope.setPage(1);
-                $scope.createMap();
+            $scope.search();
+            $scope.createMap($scope.showRequests);
+            $scope.$watch('requests + searchInput', function (newVal, oldVal) {
+                $scope.search();
+                $scope.createMap($scope.showRequests);
             });
         });
         // initialize to page 1
 
     }
 
-    function setPage(page) {
+    function setPage(page, filterItems) {
         if (page < 1 || page > $scope.pager.totalPages) {
             return;
         }
 
         // get pager object from service
-        $scope.pager = PagerService.GetPager($rootScope.requests.length, page, $scope.requestPerPage);
+        $scope.pager = PagerService.GetPager(filterItems.length, page, $scope.requestPerPage);
         // get current page of items
-        $scope.showRequests = $rootScope.requests.slice($scope.pager.startIndex, $scope.pager.endIndex + 1);
+        $scope.showRequests = filterItems.slice($scope.pager.startIndex, $scope.pager.endIndex + 1);
     }
 
+
+    $scope.filterByStatus = function (req) {
+        if (($scope.isReceived && req.statusId == 'DA_TIEP_NHAN')
+          || ($scope.isInProgress && req.statusId == 'DA_CHUYEN')
+          || ($scope.isResolved && (req.statusId == 'DA_XU_LY' || req.statusId == 'DA_DUYET'))) {
+          return true;
+        }
+        return false;
+    }
 
 });
