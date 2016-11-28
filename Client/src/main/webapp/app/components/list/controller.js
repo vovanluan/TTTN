d53292b0c08bcdf2222e84a11bf8ccc6692c9cea
Luan <vovanluan201294@gmail.com>
2016-11-28 17:53:48 +0700

    Merge with Tai

--- Client/src/main/webapp/app/components/list/controller.js
+++ Client/src/main/webapp/app/components/list/controller.js
@@ -27,7 +27,7 @@ app.controller('listViewController', function ($rootScope, $scope, $filter, requ
       return (commentRequestId==serviceRequestId);
     }
 
-    $scope.createMap = function (requests){
+    $scope.createMarkers = function (requests){
         var averageLat = 0;
         var averageLong = 0;
         $.each(requests, function(index, request) {
@@ -36,18 +36,10 @@ app.controller('listViewController', function ($rootScope, $scope, $filter, requ
         });
         averageLat /= requests.length;
         averageLong /= requests.length;
-        console.log(averageLat);
-        console.log(averageLong);
-        averageLatLong = {lat: averageLat, lng: averageLong}
+        averageLatLong = {lat: averageLat, lng: averageLong};
+        $scope.map.setCenter(averageLatLong);
         var iconBase = "assets/resources/markerIcon/";
-        $scope.map = new google.maps.Map(document.getElementById('mainMap'), {
-            zoom: 10,
-            center: averageLatLong,
-            scrollwheel: false,
-            navigationControl: false,
-            mapTypeControl: false,
-            scaleControl: false,
-        });
+
         $scope.markers = [];
         $.each(requests, function(index, request) {
           var latlng = new google.maps.LatLng(request.latitude, request.longitude);
@@ -115,12 +107,20 @@ app.controller('listViewController', function ($rootScope, $scope, $filter, requ
         });
         if (!_.isEqual(tempRequests, $scope.filteredRequests)) {
             $scope.setPage(1, $scope.filteredRequests);
-            $scope.createMap($scope.filteredRequests);
+            $scope.createMarkers($scope.filteredRequests);
         }
 
     };
 
     function initController() {
+        $scope.map = new google.maps.Map(document.getElementById('mainMap'), {
+            zoom: 10,
+            center: myLatLng,
+            scrollwheel: false,
+            navigationControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+        });
         requestManager.loadAllRequests().then(function (requests){
             $rootScope.requests = requests;
               // functions have been describe process the data for display
